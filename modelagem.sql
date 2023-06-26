ba7c5bf9aa76448bf1872855d91cf8529feef3ff
carolbalbs <carolbalbinos@hotmail.com>
2023-06-26 19:46:54 -0300

    Update modelagem.sql

--- modelagem.sql
+++ modelagem.sql
@@ -1,16 +1,16 @@
 
 --FUNCIONARIO 
+matricula NVARCHAR(7) NOT NULL
+cod_departamento NVARCHAR(5) NOT NULL
+cod_cargo NVARCHAR(4) NOT NULL
 nome NVARCHAR(30)  NOT NULL
 sexo NCHAR(1) NOT NULL
 telefone NVARCHAR(11) NOT NULL
 cpf NVARCHAR(11) NOT NULL
 data_admissao DATE NOT NULL
-matricula NVARCHAR(7) NOT NULL
-cod_cargo NVARCHAR(4) NOT NULL
-cod_departamento NVARCHAR(4) NOT NULL
 
 -- DEPARTAMENTO 
-cod_departamento  NVARCHAR(4) NOT NULL
+cod_departamento  NVARCHAR(5) NOT NULL
 nome_departamento NVARCHAR(12) NOT NULL
 
 -- CARGO 
@@ -18,50 +18,61 @@ cod_cargo NVARCHAR(4) NOT NULL
 nome_cargo NVARCHAR(12) NOT NULL
 
 -- GERENTE
-cod_cargo NCHAR(11) NOT NULL
+salario FLOAT(8) NOT NULL
+cod_cargo NCHAR(5) NOT NULL
 cod_departamento  NVARCHAR(4) NOT NULL
 matricula NVARCHAR(7) NOT NULL
-salario FLOAT(8) NOT NULL
 vale_alimentacao FLOAT(8) NOT NULL
 
 -- COORDENADOR DE ATENDIMENTO
+gratificacao FLOAT(8) NOT NULL
 cod_cargo NCHAR(11) NOT NULL
 cod_departamento  NVARCHAR(4) NOT NULL
 matricula NVARCHAR(7) NOT NULL
 salario FLOAT(8) NOT NULL
-gratificacao FLOAT(8) NOT NULL
+
 
 -- ANALISTA DE CALL CENTER
-cod_cargo NCHAR(11) NOT NULL
-cod_departamento  NVARCHAR(4) NOT NULL
-matricula NVARCHAR(7) NOT NULL
-salario FLOAT(8) NOT NULL
 especialidade NVARCHAR(12) NOT NULL
-
--- ATENDENTE DE CALL CENTER   
 cod_cargo NCHAR(11) NOT NULL
 cod_departamento  NVARCHAR(4) NOT NULL
 matricula NVARCHAR(7) NOT NULL
 salario FLOAT(8) NOT NULL
+
+
+-- ATENDENTE DE CALL CENTER 
+
 hora_extra FLOAT(8) NOT NULL
+cod_cargo NCHAR(11) NOT NULL
+cod_departamento  NVARCHAR(4) NOT NULL
+matricula NVARCHAR(7) NOT NULL
+salario FLOAT(8) NOT NULL
 
 -- CLIENTE
-
+cpf NVARCHAR(11) NOT NULL
+cod_chamada INT(7) NOT NULL
 nome NVARCHAR(30)  NOT NULL
 sexo NCHAR(1) NOT NULL
 email NVARCHAR NOT NULL
-cpf NVARCHAR(11) NOT NULL
 cep NVARCHAR(11) NOT NULL
 telefone NVARCHAR (3) NOT NULL
 cidade NVARCHAR(30) NOT NULL
-cod_chamada INT(7) NOT NULL
+
 
 
 --RECLAMAÇOES 
-cod_departamento  NVARCHAR(4) NOT NULL
-id_cliente INT(7) NOT NULL
-id_matricula INT(7) NOT NULL
 cod_chamada INT(7) NOT NULL
+cod_departamento  NVARCHAR(5) NOT NULL
+id_cliente INT(7) NOT NULL
+matricula INT(7) NOT NULL
 protocolo INT(7) NOT NULL
 
 
+--CARDINALIDADE
+
+-- Um departamento contem 1 ou mais funcionarios
+-- Um funcionario faz parte de apenas um departamento
+-- Um funcionario ocupa apenas um cargo de gerente em um departamento 
+-- Um funcionario ocupa apenas um cargo de coordenador em um departamento 
+-- Um ou mais funcionarios ocupam um ou mais cargos de analista em um departamento 
+-- Um ou mais funcionarios ocupam um ou mais cargos de atendente em um departamento 
