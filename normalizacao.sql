ce106a8d5bf958adccd9835747031434d20dd961
Ana Carolina Balbino <carolbalbinos@hotmail.com>
2023-06-27 16:21:25 -0300

    scripts para declaracao de Foreign Key

--- normalizacao.sql
+++ normalizacao.sql
@@ -1,35 +1,39 @@
 --FUNCIONARIO 
 
-ALTER TABLE  
-ADD FOREIGN KEY () REFERENCES ();
+ALTER TABLE funcionario  
+ADD  FOREIGN KEY (cod_departamento) REFERENCES departamento(cod_departamento);
 
 -- GERENTE
 
-ALTER TABLE usuario
-ADD FOREIGN KEY ()  REFERENCES ();
+ALTER TABLE gerente
+ADD FOREIGN KEY (cod_cargo)  REFERENCES cargo(cod_cargo);
 
 -- COORDENADOR DE ATENDIMENTO
 
-ALTER TABLE   
-ADD FOREIGN KEY () REFERENCES ();
+ALTER TABLE coordenador  
+ADD FOREIGN KEY (cod_cargo) REFERENCES cargo(cod_cargo);
 
 
 -- ANALISTA DE CALL CENTER
 
-ALTER TABLE   
-ADD FOREIGN KEY () REFERENCES ();
+ALTER TABLE analista 
+ADD FOREIGN KEY (cod_cargo) REFERENCES cargo(cod_cargo);
 
 -- ATENDENTE DE CALL CENTER
 
-ALTER TABLE   
-ADD FOREIGN KEY () REFERENCES ();
+ALTER TABLE atendente   
+ADD FOREIGN KEY (cod_cargo) REFERENCES cargo(cod_cargo);
 
 -- CLIENTE
 
-ALTER TABLE   
-ADD FOREIGN KEY () REFERENCES ();
+ALTER TABLE cliente  
+ADD FOREIGN KEY (telefone_cliente) REFERENCES reclamacoes(telefone_cliente);
 
 --RECLAMAÇOES
 
-ALTER TABLE   
-ADD FOREIGN KEY () REFERENCES ();
+ALTER TABLE reclamacoes  
+ADD FOREIGN KEY (cpf_cliente) REFERENCES cliente(cpf_cliente);
+
+
+ALTER TABLE reclamacoes  
+ADD FOREIGN KEY (matricula) REFERENCES atendente(matricula);
