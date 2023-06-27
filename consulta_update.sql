4f95cb92ef07f7c77514b3d7de8aeb884ebe2017
carolbalbs <carolbalbinos@hotmail.com>
2023-06-27 10:10:44 -0300

    script de consulta sql

---
+++ consulta_update.sql
@@ -0,0 +1,9 @@
+--Consulta
+-- Ao final de cada semana, criar um relatório que relatório que descreva o número de reclamações registradas e resolvidas. 
+
+SELECT  AS  , , 
+FROM  
+JOIN   ON  ;
+
+--Updates
+-- Solicitaram que seja possível transformar uma reclamação de aberto em resolvida a partir do código da reclamação.
