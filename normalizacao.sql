b75ad171328d17c3a7d1175e7c7a2ef93ee40cbb
carolbalbs <carolbalbinos@hotmail.com>
2023-06-28 14:25:12 -0300

    script para declaracao de FK

--- normalizacao.sql
+++ normalizacao.sql
@@ -1,33 +1,18 @@
---FUNCIONARIO 
-
-ALTER TABLE funcionario  
-ADD  FOREIGN KEY (cod_departamento) REFERENCES departamento(cod_departamento);
-
--- GERENTE
-
-ALTER TABLE gerente
-ADD FOREIGN KEY (cod_cargo)  REFERENCES cargo(cod_cargo);
-
--- COORDENADOR DE ATENDIMENTO
-
-ALTER TABLE coordenador  
-ADD FOREIGN KEY (cod_cargo) REFERENCES cargo(cod_cargo);
-
-
--- ANALISTA DE CALL CENTER
-
-ALTER TABLE analista 
-ADD FOREIGN KEY (cod_cargo) REFERENCES cargo(cod_cargo);
-
--- ATENDENTE DE CALL CENTER
-
-ALTER TABLE atendente   
-ADD FOREIGN KEY (cod_cargo) REFERENCES cargo(cod_cargo);
-
 -- CLIENTE
 
-ALTER TABLE cliente  
-ADD FOREIGN KEY (telefone_cliente) REFERENCES reclamacoes(telefone_cliente);
+ALTER TABLE reclamacoes
+ADD CONSTRAINT unique_telefone_cliente UNIQUE (telefone_cliente);
+
+ALTER TABLE cliente
+ADD CONSTRAINT fk_telefone_cliente FOREIGN KEY (telefone_cliente) REFERENCES reclamacoes(telefone_cliente);
+
+--FUNCIONARIO 
+
+ALTER TABLE departamento
+ADD CONSTRAINT unique_cod_departamento UNIQUE (cod_departamento);
+
+ALTER TABLE funcionario
+ADD CONSTRAINT fk_cod_departamento FOREIGN KEY (cod_departamento) REFERENCES departamento(cod_departamento);
 
 --RECLAMAÇOES
 
@@ -37,3 +22,31 @@ ADD FOREIGN KEY (cpf_cliente) REFERENCES cliente(cpf_cliente);
 
 ALTER TABLE reclamacoes  
 ADD FOREIGN KEY (matricula) REFERENCES atendente(matricula);
+
+-- UNIQUE COD_CARGO
+ALTER TABLE cargo
+ADD CONSTRAINT unique_cod_cargo UNIQUE (cod_cargo);
+
+-- GERENTE
+
+ALTER TABLE gerente
+ADD FOREIGN KEY (cod_cargo)  REFERENCES cargo(cod_cargo);
+
+-- COORDENADOR DE ATENDIMENTO
+
+
+ALTER TABLE coordenador  
+ADD FOREIGN KEY (cod_cargo) REFERENCES cargo(cod_cargo);
+
+
+-- ANALISTA DE CALL CENTER
+
+
+ALTER TABLE analista
+ADD CONSTRAINT fk_cod_cargo FOREIGN KEY (cod_cargo) REFERENCES cargo(cod_cargo);
+
+-- ATENDENTE DE CALL CENTER
+
+
+ALTER TABLE atendente
+ADD CONSTRAINT fk_cod_cargo FOREIGN KEY (cod_cargo) REFERENCES cargo(cod_cargo);
